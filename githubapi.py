7f1911cb29c47e5f67308aa0eba45928ee494723
Grace Z <gz.gracez16@gmail.com>
2015-08-19 17:25:12 -0400

    gh auth, tb

---
+++ githubapi.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import traceback, sys
+
+gh_username = raw_input('GitHub username: ')
+gh_password = raw_input('GitHub password: ')
+# payload = json.dumps({'scopes': []})
+payload = json.dumps({'scopes': [],'note': "python test auth"})
+
+gh_response = requests.post('https://api.github.com/authorizations', auth=(gh_username, gh_password), data=payload)
+try:
+    # print gh_response.json()['token']
+    print gh_response.json()
+    print gh_response.json()['token']
+except: 
+    # traceback.print_tb()
+    print (traceback.format_exc())
+    print ("Traceback printed.")
