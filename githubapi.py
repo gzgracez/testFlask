8ff382d57d96e8c255277f4d6ed658ce2a319620
Grace Z <gz.gracez16@gmail.com>
2015-08-24 17:32:05 -0400

    pw

--- githubapi.py
+++ githubapi.py
@@ -1,16 +1,17 @@
+import getpass
 import requests
 import json
 import traceback, sys
 
 gh_username = raw_input('GitHub username: ')
-gh_password = raw_input('GitHub password: ')
+gh_password = getpass.getpass('GitHub password: ')
 # payload = json.dumps({'scopes': []})
 payload = json.dumps({'scopes': [],'note': "python test auth"})
 
 gh_response = requests.post('https://api.github.com/authorizations', auth=(gh_username, gh_password), data=payload)
 try:
     # print gh_response.json()['token']
-    print gh_response.json()
+    #print gh_response.json()
     print gh_response.json()['token']
 except: 
     # traceback.print_tb()
